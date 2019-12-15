bf8ff5533de393fe15646930d505d0670557b18d
johnsong <shocking82@nate.com>
2019-12-15 16:50:17 +0900

    Activity 추가

---
+++ app/src/main/java/com/sap/kaisa/KakaoMapActivity.kt
@@ -0,0 +1,12 @@
+package com.sap.kaisa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KakaoMapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kakao_map)
+    }
+}
--- app/src/main/java/com/sap/kaisa/MainActivity.kt
+++ app/src/main/java/com/sap/kaisa/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.sap.kaisa
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+
+
+        btnKakaoMapGo.setOnClickListener {
+            var intent = Intent(applicationContext, KakaoMapActivity::class.java)
+            startActivity(intent)
+
         }
     }
 
