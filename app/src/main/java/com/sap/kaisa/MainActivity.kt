9d08ee50e245dbb7e3b051f69a4570721eea90c9
insang1983 <insang1983@gmail.com>
2020-01-05 12:28:54 +0900

    구글 광고 추가

--- app/src/main/java/com/sap/kaisa/MainActivity.kt
+++ app/src/main/java/com/sap/kaisa/MainActivity.kt
@@ -5,19 +5,28 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mAdView : AdView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        MobileAds.initialize(this)
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
         btnKakaoMapGo.setOnClickListener {
             var intent = Intent(applicationContext, KakaoMapActivity::class.java)
             startActivity(intent)
-
         }
     }
 
